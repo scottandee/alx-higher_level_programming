72fd00e68193ba611853b76ce4fe20a459270c20
Andee <andeeolayinkascott@gmail.com>
2023-03-07 19:53:26 +0000

    Task 6

---
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+for i in range(9):
+    for j in range (i, 10):
+        if i == 8 and j == 9:
+            print("89")
+            continue
+        elif i != j:
+            print("{}{}, ".format(i, j), end='')
