bc8cc930ab5fdc193320c70b0de207f7c971871a
Andee <andeeolayinkascott@gmail.com>
2023-03-08 03:19:48 +0000

    Tasak 13

--- 0x01-python-if_else_loops_functions/13-insert_number.c
+++ 0x01-python-if_else_loops_functions/13-insert_number.c
@@ -8,8 +8,9 @@
 
 listint_t *insert_node(listint_t **head, int number)
 {
-	listint_t *new_node, *temp;
-	/*int i = 0, j;*/
+	add_nodeint_end(head, number);
+	add_nodeint(head, number);
+	/*listint_t *new_node, *temp;
 
 	temp = *head;
 	new_node = malloc(sizeof(listint_t));
@@ -17,6 +18,39 @@ listint_t *insert_node(listint_t **head, int number)
 	{
 		return (NULL);
 	}
+	if (temp == NULL)
+	{
+		*head = new_node;
+	}
+	else
+	{
+		while (temp->next != NULL)
+		{
+			temp = temp->next;
+		}
+		temp->next = new_node;
+	}
+	new_node->n = number;
+	return (*head);*/
+	return (*head);
+}
+listint_t *add_nodeint(listint_t **head, const int n)
+{
+	listint_t *new_node;
+
+	new_node = (malloc(sizeof(listint_t)));
+	if (new_node == NULL)
+	{
+		return (NULL);
+	}
+	new_node->n = n;
+	new_node->next = *head;
+
+	*head = new_node;
+
+	return (*head);
+}
+
 	/*new_node->n = number;
 	new_node->next = NULL;
 
@@ -33,18 +67,4 @@ listint_t *insert_node(listint_t **head, int number)
 	new_node->next = temp;
 	return (new_node);*/
 
-	if (temp == NULL)
-	{
-		*head = new_node;
-	}
-	else
-	{
-		while (temp->next != NULL)
-		{
-			temp = temp->next;
-		}
-		temp->next = new_node;
-	}
-	new_node->n = number;
-	return (*head);
-}
+
