703da9cc8e5be8daf753588b8eabc876033c7a67
Andee <andeeolayinkascott@gmail.com>
2023-03-09 11:51:16 +0000

    Task 0

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-import add_0 as addition
+import add_0 
 a = 1
 b = 2
-print("{} + {} = {}".format(a, b, addition.add(a, b)))
+print("{} + {} = {}".format(a, b, add_0.add(a, b)))
