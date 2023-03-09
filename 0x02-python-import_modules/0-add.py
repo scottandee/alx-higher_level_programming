79f81d922fd2f11ac2124ad30b246fc6deff9823
Andee <andeeolayinkascott@gmail.com>
2023-03-09 16:45:18 +0000

    Task 0: final touches

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
-    from add_0 import add 
+    import add_0 as adds
     a = 1
     b = 2
-    print("{} + {} = {}".format(a, b, add(a, b)))
+    print("{} + {} = {}".format(a, b, adds.add(a, b)))
