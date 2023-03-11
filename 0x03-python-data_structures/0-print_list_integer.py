7dac6e208cc2ef06792303650a4c9e03c5b3b24b
Andee <andeeolayinkascott@gmail.com>
2023-03-11 09:03:18 +0000

    Task 0

--- 0x03-python-data_structures/0-print_list_integer.py
+++ 0x03-python-data_structures/0-print_list_integer.py
@@ -2,4 +2,4 @@
 def print_list_integer(my_list=[]):
     """Function that prints out the content of a list"""
     for i in range(len(my_list)):
-        print("{}".format(int(my_list[i])))
+        print("{}".format(my_list[i]))
