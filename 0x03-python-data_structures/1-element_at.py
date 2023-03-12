096ad8e95bf0a00bd2ceb713e63ad2bcb616c04e
Andee <andeeolayinkascott@gmail.com>
2023-03-12 10:38:55 +0000

    Task 1

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -3,8 +3,8 @@ def element_at(my_list, idx):
     """Function that replaces the element at the given index"""
     length = len(my_list)
     if idx < 0:
-        return None
+        return
     elif idx > length:
-        return None
+        return
     else:
         return my_list[idx]
