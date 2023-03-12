4b395f24b57756f58d007c2b47fdbe26ee8e4824
Andee <andeeolayinkascott@gmail.com>
2023-03-12 00:33:09 +0000

    Task 5

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -4,5 +4,5 @@ def no_c(my_string):
     for i in range(len(my_string)):
         if my_string[i] == 'c' or my_string[i] == 'C':
             continue
-        copy = copy +  my_string[i]
+        copy = copy + my_string[i]
     return copy
