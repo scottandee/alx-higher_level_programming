02bbaa7871dd7ccea5cd643c357ea4c341e42c02
Andee <andeeolayinkascott@gmail.com>
2023-03-27 09:43:34 +0000

    Task 0: added exception name to except

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -3,9 +3,9 @@ def safe_print_list(my_list=[], x=0):
     count = 0
     for i in range(x):
         try:
-            print("{}".format(my_list[i]), end ='')
+            print("{}".format(my_list[i]), end='')
             count = count + 1
-        except:
+        except IndexError:
             break
     print()
     return count
