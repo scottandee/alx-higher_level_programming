fde764624434575c3b7fd0d0ce05d169d3b1d5dd
Andee <andeeolayinkascott@gmail.com>
2023-03-28 02:57:04 +0000

    import stderr added

--- 0x05-python-exceptions/100-safe_print_integer_err.py
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -1,9 +1,12 @@
 #!/usr/bin/python3
+import sys
+
+
 def safe_print_integer_err(value):
     bol = True
     try:
         print("{:d}".format(value))
     except (ValueError, TypeError) as e:
-        print("Exception: {}".format(e))
+        print("Exception: {}".format(e), file=sys.stderr)
         bol = False
     return bol
