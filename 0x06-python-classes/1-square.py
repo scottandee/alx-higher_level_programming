75153fe78a47a84966a137c660870796fe5ad176
Andee <andeeolayinkascott@gmail.com>
2023-03-28 18:44:45 +0000

    Task 1: DocStrings Added

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,4 +1,28 @@
-#!/usr/bin/python3
+#!/usr/bin/pythonu3
+'''
+Class:
+    Square
+'''
+
+
 class Square:
+    '''
+    A class that creates a square
+
+    ...
+
+    Attibute
+    --------
+    size : int
+        size of the square
+    '''
     def __init__(self, size):
+        '''
+        Constructs all the necessary attributes for the square object
+
+        Parameters:
+        -----------
+            size : str
+                size of the square
+        '''
         self.__size = size
