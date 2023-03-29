d56fbc266e120cc77d993c21b303f48c9f9bfa52
Andee <andeeolayinkascott@gmail.com>
2023-03-29 01:25:38 +0000

    Task 3: Object method created

---
+++ 0x06-python-classes/3-square.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""
+This module initialises a private object variable
+with an optional size
+"""
+
+
+class Square:
+    """This is a class that creates a square"""
+
+    def __init__(self, size=0):
+        """this initialises a private object variable"""
+        if type(size) != int:
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
+
+    def area(self):
+        """This returns the area of the square"""
+        return self.__size ** 2
