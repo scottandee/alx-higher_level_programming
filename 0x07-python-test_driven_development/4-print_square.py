ab47954c2be890137d57338917364790f4565eef
Andee <andeeolayinkascott@gmail.com>
2023-04-10 02:34:29 +0000

    Function that prints a square and it's doctests

---
+++ 0x07-python-test_driven_development/4-print_square.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""Module that contains the print square function"""
+
+
+def print_square(size):
+    if type(size) == float and size > 0:
+        raise TypeError("size must be an integer")
+    if type(size) != int:
+        raise TypeError("size must be an integer")
+    if size < 0:
+        raise ValueError("size must be >= 0")
+
+    for i in range(size):
+        for j in range(size):
+            print("#", end='')
+        if i != size - 1:
+            print("")
+    print("")
