9e0999d4bda177b46920bf2e304a667c86af63ed
Andee <andeeolayinkascott@gmail.com>
2023-04-10 02:09:20 +0000

    Some fixes made

--- 0x07-python-test_driven_development/3-say_my_name.py
+++ 0x07-python-test_driven_development/3-say_my_name.py
@@ -1,5 +1,9 @@
 #!/usr/bin/python3
+"""Script that contains say_my_name"""
+
+
 def say_my_name(first_name, last_name=""):
+    """This function says the name that is entered"""
     if type(first_name) != str:
         raise TypeError("first_name must be a string")
     if type(last_name) != str:
