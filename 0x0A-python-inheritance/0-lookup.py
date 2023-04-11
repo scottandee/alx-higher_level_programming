ce79d6e0a810d915fe1df23cb0bd09246772d08a
Andee <andeeolayinkascott@gmail.com>
2023-04-11 10:56:52 +0000

    Task 0

---
+++ 0x0A-python-inheritance/0-lookup.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""This module contains a function that returns all\
+    the attributes of a class"""
+
+
+def lookup(obj):
+    """This returns all the attributes of a function in a dictionary"""
+    return obj.__dict__
