1c1b8a4155388ebe8aeab37e279ec6aa31a27122
Andee <andeeolayinkascott@gmail.com>
2023-04-11 12:05:35 +0000

    Task 6

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""
+Module in which an empty class is created
+"""
+
+
+class BaseGeometry:
+    """This is a base geomety class with object method"""
+    def area(self):
+        raise Exception(area() is not implemented)
