ec638af17161cd87135cc9bc783edbd8d483c879
Andee <andeeolayinkascott@gmail.com>
2023-04-11 05:01:50 +0000

    Task 1

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -10,14 +10,14 @@ class Rectangle:
 
     def __init__(self, width=0, height=0):
         """This function runs when a new instance is created"""
-        if width < 0:
-            raise ValueError("width must be >= 0")
-        if height < 0:
-            raise ValueError("height must be >= 0")
         if type(height) != int:
             raise TypeError("height must be an integer")
         if type(width) != int:
             raise TypeError("width must be an integer")
+         if width < 0:
+            raise ValueError("width must be >= 0")
+        if height < 0:
+            raise ValueError("height must be >= 0")
         self.__width = width
         self.__height = height
 
