689a55a3fd60f638a726daa7fd4efea17913669d
Andee <andeeolayinkascott@gmail.com>
2023-04-11 11:48:36 +0000

    Task 3

--- 0x0A-python-inheritance/2-is_same_class.py
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -1,10 +1,13 @@
 #!/usr/bin/python3
-"""This module contains a function that checks if isinstance"""
+"""
+This module contains a function that checks if isinstance
+if the object is exactly an instance of the specified class
+"""
 
 
 def is_same_class(obj, a_class):
     if a_class == object:
-         return False
+        return False
     if isinstance(obj, a_class) is True:
         return True
     return False
---
+++ 0x0A-python-inheritance/3-is_kind_of_class.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""
+This module contains a function that checks if object is
+instance of or if  if the object is an instance of a class
+that inherited from, the specified class
+"""
+
+
+def is_kind_of_class(obj, a_class):
+    if isinstance(obj, a_class) is True:
+        return True
+    return False
