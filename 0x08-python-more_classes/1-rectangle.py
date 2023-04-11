3b8162c68b4ee47f4003b25205596a702e11dd66
Andee <andeeolayinkascott@gmail.com>
2023-04-11 05:03:29 +0000

    Some fixes

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -14,7 +14,7 @@ class Rectangle:
             raise TypeError("height must be an integer")
         if type(width) != int:
             raise TypeError("width must be an integer")
-         if width < 0:
+        if width < 0:
             raise ValueError("width must be >= 0")
         if height < 0:
             raise ValueError("height must be >= 0")
