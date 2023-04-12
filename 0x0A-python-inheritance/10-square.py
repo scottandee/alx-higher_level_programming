93cc1001454e368c506952575cacb891c2f96fcb
Andee <andeeolayinkascott@gmail.com>
2023-04-12 02:47:54 +0000

    Task 10

--- 0x0A-python-inheritance/10-square.py
+++ 0x0A-python-inheritance/10-square.py
@@ -7,6 +7,9 @@ Rectangle = __import__("9-rectangle").Rectangle
 
 class Square(Rectangle):
     """This is a grandchild class that inherits the rectangle class"""
-    
+
     def __init__(self, size):
         super().__init__(size, size)
+
+    def area(self):
+        return super().area()
