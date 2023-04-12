e808a2c32ba2e7f37561fcd4433f4d0b90aff5fd
Andee <andeeolayinkascott@gmail.com>
2023-04-12 02:20:05 +0000

    Task 10

---
+++ 0x0A-python-inheritance/10-square.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""Module that contains a grandchild class"""\
+
+
+Rectangle = __import__("9-rectangle").Rectangle
+
+
+class Square(Rectangle):
+    """This is a grandchild class that inherits the rectangle class"""
+    
+    def __init__(self, size):
+        super().__init__(size, size)
