a408df21e1b87304a296068449b4a08214be3dd1
Andee <andeeolayinkascott@gmail.com>
2023-04-12 03:22:43 +0000

    Task 4

--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -6,6 +6,7 @@ for class that inherits from
 
 
 def inherits_from(obj, a_class):
+    """Checks if an object is only a subclass of"""
     if type(obj) is not a_class and issubclass(type(obj), a_class):
         return True
     return False
