9de6bac7de7a4e4679af70ec7d048d97e07bdcbb
Andee <andeeolayinkascott@gmail.com>
2023-04-12 01:22:38 +0000

    Task 6: bug fixes

--- 0x0A-python-inheritance/6-base_geometry.py
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -8,4 +8,4 @@ class BaseGeometry:
     """This is a base geomety class with object method"""
     def area(self):
         """This tells us that area is not implemented"""
-        raise Exception(area() is not implemented)
+        raise Exception("area() is not implemented")
