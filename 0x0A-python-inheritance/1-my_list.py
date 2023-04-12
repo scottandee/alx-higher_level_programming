4d09a28b6f7f7cdc981fbfabd6a3980926d79579
Andee <andeeolayinkascott@gmail.com>
2023-04-12 04:18:38 +0000

    Task 1 bug fixes

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -5,4 +5,5 @@
 class MyList(list):
     """This is an object that inherits from a class list"""
     def print_sorted(self):
+        """This prints out a sorted list"""
         print(sorted(self))
