69e68d7ace7f1c143ccf0fcaa19e3e2daddbd892
Andee <andeeolayinkascott@gmail.com>
2023-04-13 19:48:22 +0000

    Task 7: bug fix

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -13,7 +13,7 @@ load_from_json_file = __import__("6-load_from_json_file").load_from_json_file
 init_list = []
 if len(sys.argv) == 1:
     with open("add_item.json", mode="w", encoding="utf-8") as f:
-        f.write(json.dumps(init_list) + "\n")
+        f.write(json.dumps(init_list))
 
 my_list = load_from_json_file("add_item.json")
 for i in range(len(sys.argv)):
@@ -22,5 +22,3 @@ for i in range(len(sys.argv)):
     my_list.append(sys.argv[i])
 
 save_to_json_file(my_list, "add_item.json")
-with open("add_item.json", mode="a", encoding="utf-8") as f:
-    f.write("\n")
