cf3c394e08cb15378e1c578e5538b45b99ce4e97
Andee <andeeolayinkascott@gmail.com>
2023-04-13 20:05:28 +0000

    Task 7

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -6,15 +6,23 @@ a python file and saves them to a file
 
 import json
 import sys
+import os
+from os.path import exists
 save_to_json_file = __import__("5-save_to_json_file").save_to_json_file
 load_from_json_file = __import__("6-load_from_json_file").load_from_json_file
 
+# First, we'll have to get the path for add_item.json
+directory = os.getcwd() + "\\add_item.json"
 
 init_list = []
-if len(sys.argv) == 1:
+
+# Then we check if that directory exists.
+# If it doesn't, we create it and put in an empty list
+if not exists(directory):
     with open("add_item.json", mode="w", encoding="utf-8") as f:
         f.write(json.dumps(init_list))
 
+# If it does, we load and then append
 my_list = load_from_json_file("add_item.json")
 for i in range(len(sys.argv)):
     if i == 0:
