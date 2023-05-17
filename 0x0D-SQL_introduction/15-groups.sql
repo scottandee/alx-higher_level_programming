fa0f7968c002ed8bc49674122db5bbd9257f7f5f
Andee <andeeolayinkascott@gmail.com>
2023-05-17 02:15:22 +0000

    Added ORDER BY statement

--- 0x0D-SQL_introduction/15-groups.sql
+++ 0x0D-SQL_introduction/15-groups.sql
@@ -1,5 +1,6 @@
 -- This lists the number of records with
 -- the same score in the table
-SELECT score, COUNT(*) AS "number"
+SELECT score, COUNT(*) AS 'number'
 FROM second_table
-GROUP BY score;
+GROUP BY score
+ORDER BY 'number' DESC
