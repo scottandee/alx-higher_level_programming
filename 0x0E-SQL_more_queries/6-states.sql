9a642f18d4ce24c20d2f44613db815bd405dd959
Andee <andeeolayinkascott@gmail.com>
2023-05-17 13:50:24 +0000

    Task6: created database and table, implemented some constraints

---
+++ 0x0E-SQL_more_queries/6-states.sql
@@ -0,0 +1,7 @@
+-- This script creates a database and table
+-- It implements the auto_generated constraint
+CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
+USE hbtn_0d_usa;
+CREATE TABLE IF NOT EXISTS states(
+id INT UNIQUE AUTO_INCREMENT NOT NULL PRIMARY KEY
+name VARCHAR(256));
