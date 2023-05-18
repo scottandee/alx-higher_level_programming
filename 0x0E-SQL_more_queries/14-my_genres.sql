582f7ebfb77a95d649d3ff07111a757ceb866d20
Andee <andeeolayinkascott@gmail.com>
2023-05-18 05:35:15 +0000

    Task 14: Listed all genres of a particular show

---
+++ 0x0E-SQL_more_queries/14-my_genres.sql
@@ -0,0 +1,7 @@
+-- This script lists all genres of the
+-- show dexter
+SELECT tv_genres.name
+FROM tv_genres
+INNER JOIN tv_show_genres
+ON tv_genres.id = tv_show_genres.genre_id
+WHERE tv_show_genres.show_id = 8
