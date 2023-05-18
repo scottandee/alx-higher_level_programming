5b5102c71569eaa14cab0fe27ce103f39352e8e3
Andee <andeeolayinkascott@gmail.com>
2023-05-18 11:09:02 +0000

    Task 14: show id taken out and replaced with show title

--- 0x0E-SQL_more_queries/14-my_genres.sql
+++ 0x0E-SQL_more_queries/14-my_genres.sql
@@ -3,6 +3,8 @@
 SELECT tv_genres.name
 FROM tv_genres
 INNER JOIN tv_show_genres
+INNER JOIN tv_shows
 ON tv_genres.id = tv_show_genres.genre_id
-WHERE tv_show_genres.show_id = 8
-ORDER BY tv_genres.name
+ON tv_shows.id = tv_show_genres.show_id
+WHERE tv_shows.title = 'Dexter'
+ORDER BY tv_genres.name;
