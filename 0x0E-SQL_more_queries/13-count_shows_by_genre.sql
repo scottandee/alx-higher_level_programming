9bb95079e16ca100854218244036db68d33e3a66
Andee <andeeolayinkascott@gmail.com>
2023-05-18 05:07:17 +0000

    Task 13: Bug fix

--- 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
+++ 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
@@ -1,9 +1,9 @@
 -- This script counts the numbers of 
 -- shows that are linked to each genres
-SELECT tv_genres.name AS 'genre',
-COUNT(tv_show_genres.genre_id) AS 'number of shows'
+SELECT tv_genres.name AS genre,
+COUNT(tv_show_genres.genre_id) AS number_of_shows
 FROM tv_genres
 INNER JOIN tv_show_genres
 ON tv_show_genres.genre_id = tv_genres.id
 GROUP BY tv_genres.name, tv_show_genres.genre_id
-ORDER BY 'number of shows' DESC
+ORDER BY number_of_shows DESC; 
