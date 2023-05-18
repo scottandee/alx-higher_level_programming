75a3f62193a474fca405f322aef9e84ab3fe199b
Andee <andeeolayinkascott@gmail.com>
2023-05-18 11:34:18 +0000

    Reverted test

--- 0x0E-SQL_more_queries/14-my_genres.sql
+++ 0x0E-SQL_more_queries/14-my_genres.sql
@@ -2,8 +2,8 @@
 -- show dexter
 SELECT tv_genres.name
 FROM tv_genres
-LEFT JOIN tv_show_genres
-LEFT JOIN tv_shows
+INNER JOIN tv_show_genres
+INNER JOIN tv_shows
 ON tv_shows.id = tv_show_genres.show_id
 ON tv_genres.id = tv_show_genres.genre_id
 WHERE tv_shows.title = 'Dexter'
