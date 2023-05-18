d3bfaffda9929b7d7bf09381b8cde06fc4e823b5
Andee <andeeolayinkascott@gmail.com>
2023-05-18 04:01:38 +0000

    Task 12: Listed all shows with genre as NULL

---
+++ 0x0E-SQL_more_queries/12-no_genre.sql
@@ -0,0 +1,8 @@
+-- This lists out all the shows
+-- that have no genre linked
+SELECT tv_shows.title, tv_show_genres.genre_id
+FROM tv_shows
+LEFT OUTER JOIN tv_show_genres
+ON tv_shows.id = tv_show_genres.show_id
+WHERE tv_show_genres.show_id IS NULL
+ORDER BY tv_shows.title, tv_show_genres.genre_id
