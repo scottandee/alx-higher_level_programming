b2b9070cd6f0fe48f73809de92d927e354a244e5
Andee <andeeolayinkascott@gmail.com>
2023-05-23 16:02:21 +0000

    Changed to Subquery Approach

--- 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
+++ 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
@@ -2,5 +2,5 @@
 -- carlifonia that can be found in the database
 SELECT id, name
 FROM cities
-WHERE state_id = 1
+WHERE cities.state_id = (SELECT id FROM states WHERE name = 'California')
 ORDER BY cities.id 
