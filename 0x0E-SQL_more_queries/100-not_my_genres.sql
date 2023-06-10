a50bd2e0e91798f9edae4d4c0fd930092baf763b
Andee <andeeolayinkascott@gmail.com>
2023-06-10 15:52:37 +0000

    Add order by clause to task 100

--- 0x0E-SQL_more_queries/100-not_my_genres.sql
+++ 0x0E-SQL_more_queries/100-not_my_genres.sql
@@ -9,4 +9,5 @@ WHERE tv_genres.name NOT IN (
 	LEFT JOIN tv_shows
 	ON tv_shows.id = tv_show_genres.show_id
 	ON tv_genres.id = tv_show_genres.genre_id
-	WHERE tv_shows.title = 'Dexter'); 
+	WHERE tv_shows.title = 'Dexter')
+ORDER BY tv_genres.name; 
