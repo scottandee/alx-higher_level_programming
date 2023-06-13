160281a9db35d0b48c5b67e6df2153e7dbf0a06a
Olayinkascott Andee <andeeolayinkascott@gmail.com>
2023-06-13 16:05:58 +0100

    Print with new lines

---
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -0,0 +1,8 @@
+#!/usr/bin/node
+
+const cLang = 'C is fun';
+const pyLang = 'Python is cool';
+const jsLang = 'Javasrcipt is amazing';
+console.log(cLang);
+console.log(pyLang);
+console.log(jsLang);
