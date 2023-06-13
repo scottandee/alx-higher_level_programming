3e60eda1079023ef918d9ad2c92cbbdeb71f6ba7
Olayinkascott Andee <andeeolayinkascott@gmail.com>
2023-06-13 16:08:40 +0100

    refactor code

--- 0x12-javascript-warm_up/1-multi_languages.js
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -1,8 +1,3 @@
 #!/usr/bin/node
 
-const cLang = 'C is fun';
-const pyLang = 'Python is cool';
-const jsLang = 'Javasrcipt is amazing';
-console.log(cLang);
-console.log(pyLang);
-console.log(jsLang);
+console.log('C is fun\nPython is cool\nJavaScript is amazing');
