925aa82bb1631f28e73f78dd72a6e0f48fb3d8da
Andee <andeeolayinkascott@gmail.com>
2023-06-14 04:09:46 +0000

    Print out second largest

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -1,7 +1,16 @@
 #!/usr/bin/node
-// This script searches the second biggest integer in the list of arguments
 
 import { argv } from 'node:process';
 
-:wq
-
+const numbers = [];
+if (argv.length === 2 || argv.length === 3) {
+  console.log('0');
+} else {
+  for (let i = 2; i < argv.length; i++) {
+    numbers.push(argv[i]);
+  }
+  numbers.sort(function (a, b) {
+    return a - b;
+  });
+  console.log(numbers[numbers.length - 2]);
+}
