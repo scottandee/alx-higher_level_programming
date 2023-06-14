c5e59c25a4684455c5d823f7798f778503dde24e
Olayinkascott Andee <andeeolayinkascott@gmail.com>
2023-06-14 04:00:28 +0100

    revert previous change

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
 let i;
-const langs = ["C is fun", "Python is coo", "JavaScript is amazing"];
+const langs = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
 for (i = 0; i < 3; i++) {
   console.log(langs[i]);
 }
