96bd518d5c921b4d01a188ead583e37f83c6d3ca
Olayinkascott Andee <andeeolayinkascott@gmail.com>
2023-06-14 04:10:19 +0100

    Revert changes

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -1,6 +1,7 @@
 #!/usr/bin/node
-let i;
+
 const langs = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
-for (i = 0; i < 3; i++) {
+
+for (let i = 0; i < langs.length; i++) {
   console.log(langs[i]);
 }
