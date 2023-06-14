896f19225e1a43c922d60c8f46270915d0aa9b95
Olayinkascott Andee <andeeolayinkascott@gmail.com>
2023-06-14 03:33:41 +0100

    remove comment

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -1,5 +1,4 @@
 #!/usr/bin/node
-// This script loops through an array and prints each member of the array
 const langs = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
 for (let i = 0; i < langs.length; i++) {
   console.log(langs[i]);
