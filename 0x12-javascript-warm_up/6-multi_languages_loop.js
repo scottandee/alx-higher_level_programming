29a8220d8963ff8b380bf016e241b49fb645830d
Olayinkascott Andee <andeeolayinkascott@gmail.com>
2023-06-14 02:28:35 +0100

    use fewer lines

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -1,8 +1,6 @@
 #!/usr/bin/node
 // This script loops through an array and prints each member of the array
-
 const langs = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
-
 for (let i = 0; i < langs.length; i++) {
   console.log(langs[i]);
 }
