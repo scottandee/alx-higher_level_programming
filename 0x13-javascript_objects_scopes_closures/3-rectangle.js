3992675e4e2fa464953504b35f83bb2ff902a1e9
Olayinkascott Andee <andeeolayinkascott@gmail.com>
2023-06-15 02:59:05 +0100

    change import method

--- 0x13-javascript_objects_scopes_closures/3-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/3-rectangle.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
 
-module.exports = class Rectangle {
+class Rectangle {
   constructor (w, h) {
     if (w > 0 && h > 0) {
       this.width = w;
@@ -18,3 +18,5 @@ module.exports = class Rectangle {
     }
   }
 };
+
+module. exports = Rectangle;
