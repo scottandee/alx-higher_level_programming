b506bb7d031222d8c84f0a085f86be3ae6c41d81
Andee <andeeolayinkascott@gmail.com>
2023-06-16 11:15:49 +0000

    query database with MySQLdb

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -7,15 +7,15 @@ import sys
 import MySQLdb
 
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     db = MySQLdb.connect(host="localhost", port=3306,
                          user="{}".format(sys.argv[1]),
                          passwd="{}".format(sys.argv[2]),
                          db="{}".format(sys.argv[3]))
 
     cur = db.cursor()
-
-    cur.execute("SELECT * FROM states ORDER BY states.id")
+    cur.execute("""SELECT * FROM states WHERE LEFT(states.name, 1)
+                = %s ORDER BY states.id""", 'N')
     rows = cur.fetchall()
     for row in rows:
         print(row)
--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -7,15 +7,14 @@ import sys
 import MySQLdb
 
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     db = MySQLdb.connect(host="localhost", port=3306,
                          user="{}".format(sys.argv[1]),
                          passwd="{}".format(sys.argv[2]),
                          db="{}".format(sys.argv[3]))
 
     cur = db.cursor()
-
-    cur.execute("SELECT * FROM states ORDER BY states.id")
+    cur.execute("SELECT * FROM states WHERE states.name = %s", [sys.argv[4]])
     rows = cur.fetchall()
     for row in rows:
         print(row)
