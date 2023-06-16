bb3285ef3d766b75ed9960babf3942d8027e6db3
Andee <andeeolayinkascott@gmail.com>
2023-06-16 03:01:00 +0000

    concat two files

---
+++ 0x13-javascript_objects_scopes_closures/102-concat.js
@@ -0,0 +1,23 @@
+#!/usr/bin/node
+
+const fs = require('fs');
+fs.readFile(process.argv[2], 'utf8', (err, data) => {
+  if (err) {
+    return;
+  }
+  fs.writeFile(process.argv[4], data, err => {
+    if (err) {
+      console.log(err);
+    }
+  });
+});
+fs.readFile(process.argv[3], 'utf8', (err, data) => {
+  if (err) {
+    return;
+  }
+  fs.appendFile(process.argv[4], data, err => {
+    if (err) {
+      console.log(err);
+    }
+  });
+});
