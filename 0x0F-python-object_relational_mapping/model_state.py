10fdeef5bb857965d70d309834773c2a6b3dde5b
Andee <andeeolayinkascott@gmail.com>
2023-06-17 07:49:23 +0000

    class that inherits from declaratuve base

---
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""This script contains the State class declaration"""
+
+
+from sqlalchemy import Column, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
+
+
+Base = declarative_base()
+
+
+class State(Base):
+    """This is the states class"""
+
+    __tablename__ = "states"
+    id = Column("id", Integer(), primary_key=True, nullable=False)
+    name = Column("name", String(128), nullable=False)
