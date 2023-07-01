defd3cfce6a62c0fd3adf4e0ac95f4eda58b3fab
Andee_Vagrant <andeeolayinkascott@gmail.com>
2023-07-01 10:33:29 +0000

    Fetch a URL

---
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+'''This script fetches a url'''
+
+
+import urllib.request
+
+
+if __name__ == "__main__":
+    url = "https://alx-intranet.hbtn.io/status"
+    result = ''
+
+    with urllib.request.urlopen(url) as response:
+        result = response.read()
+
+    utf8 = result.decode("UTF-8")
+    print("Body response:")
+    print("    - type: {}\n    - content: {}\n    - utf8 content: {}".format(
+          type(result), result, utf8))
