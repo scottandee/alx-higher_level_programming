e560e42e07c1e1a9246f643cc8867fc4e4e0c9bc
Olayinkascott Andee <andeeolayinkascott@gmail.com>
2023-09-01 15:31:20 +0100

    Fix uppercase function

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -1,5 +1,8 @@
 #!/usr/bin/python3
 def uppercase(str):
     for i in range(len(str)):
-        print("{}".format(chr(ord(str[i]) - 32)), end='')
+        if ord(str[i]) >= 97 and ord(str[i]) < 123:
+            print("{}".format(chr(ord(str[i]) - 32)), end='')
+            continue
+        print("{}".format(str[i]), end='')
     print("")
