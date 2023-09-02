fde5947140abfca03dc3ed765cf6d9cabfecd347
Olayinkascott Andee <andeeolayinkascott@gmail.com>
2023-09-02 18:56:17 +0100

    Calculate weighted average

--- 0x04-python-more_data_structures/100-weight_average.py
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -1,5 +1,11 @@
 #!/usr/bin/python3
+
 def weight_average(my_list=[]):
+    sums = 0
+    weights = 0
+    if len(my_list) == 0:
+        return 0
     for i in my_list:
-        sums = lambda x, y: x * y, i[0], i[1]
-    print(sums)
+        sums = sums + (i[0] * i[1])
+        weights = weights + i[1]
+    return sums / weights
