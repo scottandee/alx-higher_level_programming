9c7fef783032ba9b0ff331d565fb10d74dabc166
Olayinkascott Andee <andeeolayinkascott@gmail.com>
2023-09-02 11:09:47 +0100

    Edit check for underscore

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -3,6 +3,6 @@ if __name__ == "__main__":
     import hidden_4
     names = sorted(dir(hidden_4))
     for i in range(len(names)):
-        if names[i].startswith("_"):
+        if names[i].startswith("__"):
             continue
         print(names[i])
