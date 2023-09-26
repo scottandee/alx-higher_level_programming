1e44fd7dfa20f5be7d17ec2e215019c984ca092e
Olayinkascott Andee <andeeolayinkascott@gmail.com>
2023-09-26 16:17:34 +0100

    Count movies that feature Wedge Antilles

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,8 +1,13 @@
 #!/usr/bin/python3
+
 if __name__ == "__main__":
     import hidden_4
+
     names = sorted(dir(hidden_4))
+    print(names)
+
     for i in range(len(names)):
         if names[i].startswith("__"):
             continue
+
         print(names[i])
